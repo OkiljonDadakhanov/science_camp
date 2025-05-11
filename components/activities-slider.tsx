25a808b275583bb7861832ff42266a9146d05044
Okiljon <okiljondadakhonov@gmail.com>
2025-05-11 14:28:22 +0500

    robo

--- components/activities-slider.tsx
+++ components/activities-slider.tsx
@@ -11,7 +11,7 @@ import { Button } from "@/components/ui/button";
 export default function ActivitiesSlider() {
   const activities = [
     {
-      title: "Robotics Coding",
+      title: "Robo Coding",
       description:
         "Build and program robots to complete challenges and solve real-world problems.",
       image: "/images/robotics.jpg",
