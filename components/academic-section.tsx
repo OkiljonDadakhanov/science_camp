200d071427eedd962665e720f5515f989afc4a4c
Okiljon <okiljondadakhonov@gmail.com>
2025-05-11 16:31:08 +0500

    olim and asliddin pics changed

--- components/academic-section.tsx
+++ components/academic-section.tsx
@@ -25,12 +25,10 @@ export default function AcademicSection() {
       icon: "/dna-helix-icon.png",
     },
     {
-  name: "Raketashunoslik",
-  description: "Raketalar qanday ishlashini o‘rganing, ularni loyihalash va uchirish asoslarini tushuning hamda koinotga sayohat qilish imkoniyatlari bilan tanishing.",
-  icon: "/celestial-symbols.png"
-}
-,
-   
+      name: "Raketashunoslik",
+      description: "Raketalar qanday ishlashini o'rganing, ularni loyihalash va uchirish asoslarini tushuning hamda koinotga sayohat qilish imkoniyatlari bilan tanishing.",
+      icon: "/celestial-symbols.png"
+    },
     {
       name: "Kompyuter savodxonligi",
       description: "Dasturlash, algoritmlar va hisoblash fikrlashni kodlash loyihalari orqali o'rganing.",
@@ -74,22 +72,21 @@ export default function AcademicSection() {
                 image: "/mentors/firdavs.jpg",
               },
               {
-  name: "Fayyoz Tuychiyev",
-  role: "Robototexnika bo‘yicha mentor",
-  education: `Sankt-Peterburg davlat elektrotexnika universiteti "LETI" bitiruvchisi (elektronika va nanoelektronika), 2021-yil;
+                name: "Fayyoz Tuychiyev",
+                role: "Robototexnika bo'yicha mentor",
+                education: `Sankt-Peterburg davlat elektrotexnika universiteti "LETI" bitiruvchisi (elektronika va nanoelektronika), 2021-yil;
 Toshkent davlat texnika universiteti magistri (elektronika), 2023-yil`,
-  position: "Robototexnika bo‘yicha mentor",
-  achievements: [
-    { year: "2017", award: "“Kelajak muhandislari” olimpiadasi g‘olibi (Sankt-Peterburg davlat elektrotexnika universiteti tomonidan)" },
-    { year: "2018", award: "Sankt-Peterburg shahri universitetlari o‘rtasida o‘tkazilgan fizika fan olimpiadasi – 3-o‘rin" },
-    { year: "2019", award: "“Elektrotexnikaning nazariy asoslari” universitet fan olimpiadasi sovrindori" },
-    { year: "2021", award: "Butun Rossiya “Mobil Aloqa Sistemalarining Innovatsion Texnologiyalari” olimpiadasi g‘olibi" },
-    { year: "2025", award: "Maktablar uchun robototexnika bo‘yicha o‘quv qo‘llanma muallifi" },
-    { year: "2021-2025", award: "Robototexnika va elektronika bo‘yicha 10 ga yaqin ilmiy maqola va tezislar muallifi" },
-  ],
-  image: "/mentors/fayyoz.jpg",
-},
-             
+                position: "Robototexnika bo'yicha mentor",
+                achievements: [
+                  { year: "2017", award: "Kelajak muhandislari olimpiadasi g'olibi (Sankt-Peterburg davlat elektrotexnika universiteti tomonidan)" },
+                  { year: "2018", award: "Sankt-Peterburg shahri universitetlari o'rtasida o'tkazilgan fizika fan olimpiadasi – 3-o'rin" },
+                  { year: "2019", award: "Elektrotexnikaning nazariy asoslari universitet fan olimpiadasi sovrindori" },
+                  { year: "2021", award: "Butun Rossiya Mobil Aloqa Sistemalarining Innovatsion Texnologiyalari olimpiadasi g'olibi" },
+                  { year: "2025", award: "Maktablar uchun robototexnika bo'yicha o'quv qo'llanma muallifi" },
+                  { year: "2021-2025", award: "Robototexnika va elektronika bo'yicha 10 ga yaqin ilmiy maqola va tezislar muallifi" },
+                ],
+                image: "/mentors/fayyoz.jpg",
+              },
               {
                 name: "Saida Abdullaeyeva",
                 role: "Kimyo mentori",
@@ -106,79 +103,79 @@ Toshkent davlat texnika universiteti magistri (elektronika), 2023-yil`,
                 image: "/mentors/saida.JPG",
               },
               {
-  name: "Asliddin Fayzullaev",
-  role: "Fizika mentori",
-  education: "Oʻzbekiston Milliy Universiteti, Fizika fakulteti magistranti (2024–hozir)",
-  position: "I toifali o‘qituvchi",
-  achievements: [
-    { year: "2025", award: "O‘qituvchilar oʻrtasida Respublika fizika olimpiadasida 1-oʻrin" },
-    { year: "2025", award: "I toifa unvoni berilgan" },
-    { year: "2024", award: "GRE Physics xalqaro sertifikati – 900/990 ball" },
-    { year: "2020–2024", award: "OʻzMU Fizika fakulteti talabasi – GPA 92 ball" },
-    { year: "2018", award: "Innovatsion Respublika fizika olimpiadasida 1-oʻrin" },
-  ],
-  image: "/mentors/asliddin.jpg"
-},
-{
-  name: "Olimjon Olimov",
-  role: "Matematika fanidan mentor",
-  education: "Yangi O‘zbekiston Universiteti, Dasturiy ta'minot yo‘nalishi mutaxassisi",
-  position: "Geometriya bo‘yicha O‘zbekiston milliy jamoalari treneri",
-  achievements: [
-    { year: "2023", award: "ICPC Semifinal Astana musobaqasida bronza medal sovrindori" },
-    { year: "2024", award: "Xalqaro Zhautikov Olimpiadasida jamoa yetakchisi (Qozog‘iston, Olmaota)" },
-    { year: "2024", award: "Xalqaro Matematika Musobaqasida O‘zbekiston milliy jamoasi rahbari (Hindiston)" },
-    { year: "2024", award: "Junior Balkan Mathematics Olympiada milliy jamoa rahbari (Turkiya)" },
-    { year: "2024", award: "Xalqaro Al-Xorazmiy Olimpiadasi, Respublika Olimpiadalari va xalqaro olimpiadalarda hakam" },
-    { year: "2022–2024", award: "O‘zbekiston milliy jamoalari uchun geometriya bo‘yicha trener" },
-    { year: "2024", award: "Asos Akademiya o‘quv markaziga asos solgan" }
-  ],
-  image: "/mentors/olim.jpg"
-},
-
+                name: "Asliddin Fayzullaev",
+                role: "Fizika mentori",
+                education: "Oʻzbekiston Milliy Universiteti, Fizika fakulteti magistranti (2024–hozir)",
+                position: "I toifali o'qituvchi",
+                achievements: [
+                  { year: "2025", award: "O'qituvchilar oʻrtasida Respublika fizika olimpiadasida 1-oʻrin" },
+                  { year: "2025", award: "I toifa unvoni berilgan" },
+                  { year: "2024", award: "GRE Physics xalqaro sertifikati – 900/990 ball" },
+                  { year: "2020–2024", award: "OʻzMU Fizika fakulteti talabasi – GPA 92 ball" },
+                  { year: "2018", award: "Innovatsion Respublika fizika olimpiadasida 1-oʻrin" },
+                ],
+                // Fixed the image path - using uppercase extension
+                image: "/mentors/asliddin.JPG",
+              },
+              {
+                name: "Olimjon Olimov",
+                role: "Matematika fanidan mentor",
+                education: "Yangi O'zbekiston Universiteti, Dasturiy ta'minot yo'nalishi mutaxassisi",
+                position: "Geometriya bo'yicha O'zbekiston milliy jamoalari treneri",
+                achievements: [
+                  { year: "2023", award: "ICPC Semifinal Astana musobaqasida bronza medal sovrindori" },
+                  { year: "2024", award: "Xalqaro Zhautikov Olimpiadasida jamoa yetakchisi (Qozog'iston, Olmaota)" },
+                  { year: "2024", award: "Xalqaro Matematika Musobaqasida O'zbekiston milliy jamoasi rahbari (Hindiston)" },
+                  { year: "2024", award: "Junior Balkan Mathematics Olympiada milliy jamoa rahbari (Turkiya)" },
+                  { year: "2024", award: "Xalqaro Al-Xorazmiy Olimpiadasi, Respublika Olimpiadalari va xalqaro olimpiadalarda hakam" },
+                  { year: "2022–2024", award: "O'zbekiston milliy jamoalari uchun geometriya bo'yicha trener" },
+                  { year: "2024", award: "Asos Akademiya o'quv markaziga asos solgan" }
+                ],
+                // Fixed the image path - using uppercase extension and filename
+                image: "/mentors/Olim.JPG",
+              },
               {
                 name: "Sardor Qurbonboyev",
-                role: "Biologiya bo‘yicha Mentor",
+                role: "Biologiya bo'yicha Mentor",
                 education: "ITMA Abu Ali Ibn Sino nomidagi ixtisoslashtirilgan maktab bitiruvchisi (2024)",
                 position: "Hozirda Toshkent Tibbiyot Akademiyasi 1-kurs talabasi",
                 achievements: [
                   { year: "", award: "Abu Ali Ibn Sino nomidagi Xalqaro Biologiya Olimpiadasi kumush medal sohibi" },
-                  { year: "", award: " STEM olimpiadasining Respublika bosqichi 1-o‘rin g‘olibi" },
-                  { year: "", award: "Biologiya fanidan Asosiy Olimpiadaning viloyat bosqichi 1-o‘rin egasi" },
+                  { year: "", award: " STEM olimpiadasining Respublika bosqichi 1-o'rin g'olibi" },
+                  { year: "", award: "Biologiya fanidan Asosiy Olimpiadaning viloyat bosqichi 1-o'rin egasi" },
                   { year: "", award: "Abu Ali Ibn Sino tayanch maktabida biologiya fanidan olimpiadaga tayyorlovchi trener" },
                   { year: "", award: "35-Xalqaro biologiya olimpaidasi ishtirokchisi " },
-                   { year: "", award: "O‘quvchilarga sitologiya, molekulyar biologiya, fiziologiya, genetika, ekologiya va bioinformatika fanlari bo‘yicha chuqurlashtirilgan bilim beradi " },
+                  { year: "", award: "O'quvchilarga sitologiya, molekulyar biologiya, fiziologiya, genetika, ekologiya va bioinformatika fanlari bo'yicha chuqurlashtirilgan bilim beradi " },
                 ],
                 additionalInfo: "Navoiy shahridagi 11-AFCHOʻIM maktabini 2024-yilda tamomlagan",
                 image: "/mentors/sardor.jpg",
               },
-
               {
-  name: "Otabek Hasanov O'tkir o'g'li",
-  role: "Fizika va Fizika Olimpiada Mentor",
-  education: "Moskva Fizika-Texnika Instituti (MFTI), Fizika bo'yicha, QS reytingida top 50 talikka kiradi",
-  position: "2-toifa o'qituvchi",
-  achievements: [
-    { year: "2018", award: "Xalqaro Mirzo Ulug'bek olimpiadasida fizika fanidan kumush medal (IMUO 2018)" },
-    { year: "2018", award: "IPHO olimpiadasi ishtirokchisi" },
-    { year: "2019", award: "Xalqaro Jautikov olimpiadasida fizika fanidan bronza medal (IZHO 2019)" },
-    { year: "2019", award: "Respublika fizika olimpiadasida 1-o'rin" },
-    { year: "2019", award: "IPHO olimpiadasi ishtirokchisi" },
-    { year: "2019", award: "Fizteh olimpiadasida 1-darajali diplom sohibi" },
-    { year: "2025", award: "Respublika o'qituvchilar o'rtasida o'tkazilgan fizika fanidan olimpiadada 2-o'rin" }
-  ],
-  articles: [
-    { title: "Нерадиальные потоки в солнечном ветре", type: "Maqola" }
-  ],
-  certifications: [
-    { name: "Ingliz tili B2 (IELTS)" }
-  ],
-  languageSkills: [
-    { language: "Rus tili", level: "C1" },
-    { language: "Ingliz tili", level: "B2" }
-  ],
-  image: "/mentors/Otabek.jpg"
-},
+                name: "Otabek Hasanov O'tkir o'g'li",
+                role: "Fizika va Fizika Olimpiada Mentor",
+                education: "Moskva Fizika-Texnika Instituti (MFTI), Fizika bo'yicha, QS reytingida top 50 talikka kiradi",
+                position: "2-toifa o'qituvchi",
+                achievements: [
+                  { year: "2018", award: "Xalqaro Mirzo Ulug'bek olimpiadasida fizika fanidan kumush medal (IMUO 2018)" },
+                  { year: "2018", award: "IPHO olimpiadasi ishtirokchisi" },
+                  { year: "2019", award: "Xalqaro Jautikov olimpiadasida fizika fanidan bronza medal (IZHO 2019)" },
+                  { year: "2019", award: "Respublika fizika olimpiadasida 1-o'rin" },
+                  { year: "2019", award: "IPHO olimpiadasi ishtirokchisi" },
+                  { year: "2019", award: "Fizteh olimpiadasida 1-darajali diplom sohibi" },
+                  { year: "2025", award: "Respublika o'qituvchilar o'rtasida o'tkazilgan fizika fanidan olimpiadada 2-o'rin" }
+                ],
+                articles: [
+                  { title: "Нерадиальные потоки в солнечном ветре", type: "Maqola" }
+                ],
+                certifications: [
+                  { name: "Ingliz tili B2 (IELTS)" }
+                ],
+                languageSkills: [
+                  { language: "Rus tili", level: "C1" },
+                  { language: "Ingliz tili", level: "B2" }
+                ],
+                image: "/mentors/Otabek.jpg"
+              },
             ].map((mentor, i) => (
               <Card key={i} className="overflow-hidden transition-all duration-300 hover:shadow-lg">
                 <div className="aspect-square relative">
